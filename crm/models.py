86e32d4205097db42714225ea26032a42c5cdf69
g3ck0 <simane988@gmail.com>
2020-10-10 13:10:05 +0300

    Edited models.py

--- crm/models.py
+++ crm/models.py
@@ -3,13 +3,21 @@ from django.db import models
 
 # Create your models here.
 
+
 class Student(models.Model):
+    sex_types = (
+        ('M', 'Man'),
+        ('W', 'Woman')
+    )
     name = models.CharField(max_length=30, null=True)
     surname = models.CharField(max_length=30, null=True)
-    room = models.IntegerField(null=True)
+    dadname = models.CharField(max_length=30, null=True)
     email = models.EmailField(null=True)
     description = models.TextField(default="")
     photo = models.FilePathField(null=True)
+    sex = models.CharField(max_length=1, null=True, choices=sex_types)
+    weight = models.FloatField(null=True)
+    phone_number = models.CharField(max_length=12, null=True)
 
     def __str__(self):
         return self.name + " " + self.surname
